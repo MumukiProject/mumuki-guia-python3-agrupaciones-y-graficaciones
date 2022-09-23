bcba5e1626a3b470a6801e336603399d208052b7
franco@mumuki.org <franco@mumuki.org>
2022-09-23 05:00:07 +0000

    Mumuki Export on 2022-09-23 05:00:07 +0000

--- 00009_¿Series en el cine_/test.py
+++ 00009_¿Series en el cine_/test.py
@@ -1,10 +1,10 @@
 class Test(unittest.TestCase):
 
   def test_pantallas_por_sector_es_un_series(self):
-    self.assertEquals(type(provincias_con_cines_grandes), pd.Series)
+    self.assertEquals(type(butacas_por_provincia), pd.Series)
     
   def test_genera_el_series_correcto(self):
-    self.assertEquals(provincias_con_cines_grandes.to_dict(), [
+    self.assertEquals(butacas_por_provincia.to_dict(), [
          {'province': 'Buenos Aires', 'seats': 5180},
          {'province': 'Ciudad Autónoma de Buenos Aires', 'seats': 306},
          {'province': 'Salta', 'seats': 354},
