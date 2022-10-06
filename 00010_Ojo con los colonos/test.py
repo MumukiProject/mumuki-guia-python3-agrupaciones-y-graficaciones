72232ca29548dc42493275e86b67848c9bdca04c
franco@mumuki.org <franco@mumuki.org>
2022-10-06 23:07:47 +0000

    Mumuki Export on 2022-10-06 23:07:47 +0000

--- 00010_Ojo con los colonos/test.py
+++ 00010_Ojo con los colonos/test.py
@@ -1,7 +1,7 @@
 class Test(unittest.TestCase):
 
   def test_pantallas_por_sector_es_un_series(self):
-    self.assertEquals(type(provincias_con_cines_grandes), pd.Series)
+    self.assertEquals(type(butacas_por_localidad), pd.DataFrame)
     
   def test_genera_el_series_correcto(self):
     self.assertEquals(provincias_con_cines_grandes.to_dict(), {
