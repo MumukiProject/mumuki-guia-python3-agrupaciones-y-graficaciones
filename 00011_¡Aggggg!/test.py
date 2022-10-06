0eedf283eaf88f1aa9426e850e0c6fb21b7a91f8
franco@mumuki.org <franco@mumuki.org>
2022-10-06 23:56:34 +0000

    Mumuki Export on 2022-10-06 23:56:34 +0000

--- 00011_¡Aggggg!/test.py
+++ 00011_¡Aggggg!/test.py
@@ -6,6 +6,9 @@ class Test(unittest.TestCase):
   def test_genera_el_DataFrame_correcto(self):
     indexados = proporcion_cines_comeriales_provinciales.set_index("province")
     self.assertTrue(len(proporcion_cines_comeriales_provinciales) == 5, "debe tener el tamaño correcto")    
+    self.assertTrue(indexados.loc["San Juan", "sector"] == 1, "debe tener el valor correcto para San Juan")
+    self.assertTrue(indexados.loc["Buenos Aires", "sector"] == 1, "debe tener el valor correcto para Buenos Aires")
+    self.assertTrue(indexados.loc["Santa Fe", "sector"] == 0.8, "debe tener el valor correcto para Santa Fe")
     
   def test_genera_las_columnas_correctas(self):  
     self.assertEquals(list(proporcion_cines_comeriales_provinciales.columns), ['province', 'sector'])    
