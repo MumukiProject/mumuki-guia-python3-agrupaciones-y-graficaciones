b7467fe54aa647b9a46d55aeae46d7b9a6e32b82
franco@mumuki.org <franco@mumuki.org>
2022-10-06 23:47:14 +0000

    Mumuki Export on 2022-10-06 23:47:14 +0000

--- 00011_¡Aggggg!/test.py
+++ 00011_¡Aggggg!/test.py
@@ -2,3 +2,7 @@ class Test(unittest.TestCase):
 
   def test_proporcion_cines_comeriales_provinciales_es_un_DataFrame(self):
     self.assertEquals(type(proporcion_cines_comeriales_provinciales), pd.DataFrame)
+    
+  def test_genera_el_DataFrame_correcto(self):
+    indexadas = proporcion_cines_comeriales_provinciales.set_index("province")
+    self.assertTrue(len(proporcion_cines_comeriales_provinciales) == 4, "debe tener el tamaño correcto")    
