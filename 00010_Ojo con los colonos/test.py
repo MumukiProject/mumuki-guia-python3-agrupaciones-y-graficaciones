7e39c6eed4b1d3d00215d171ff0a278e2d9ae72e
franco@mumuki.org <franco@mumuki.org>
2022-11-11 16:38:17 +0000

    Mumuki Export on 2022-11-11 16:38:17 +0000

--- 00010_Ojo con los colonos/test.py
+++ 00010_Ojo con los colonos/test.py
@@ -5,10 +5,13 @@ class Test(unittest.TestCase):
     
   def test_genera_el_DataFrame_correcto(self):
     indexadas = butacas_por_localidad.set_index("city")
-    self.assertTrue(len(butacas_por_localidad) == 9, "debe tener el tamaño correcto")
+    self.assertTrue(len(butacas_por_localidad) == 2, "debe tener el tamaño correcto")
     self.assertTrue(indexadas.loc["Bragado", "seats"] == 750, "Tiene la cantidad correcta de Bragado")
     self.assertTrue(indexadas.loc["Salta", "seats"] == 354, "Tiene la cantidad correcta de Salta")
     self.assertTrue(indexadas.loc["Pilar", "seats"] == 1407, "Tiene la cantidad correcta de Pilar")
-    
+
+  def test_tiene_dos_localidades_colon(self):
+    assert len(butacas_por_localidad[butacas_por_localidad["city"]== "Colón"]) == 2, "debería tener dos localidades (city) Colón"
+  
   def test_genera_las_columnas_correctas(self):  
     self.assertEquals(set(butacas_por_localidad.columns), {'province', 'department', 'city', 'seats'})
